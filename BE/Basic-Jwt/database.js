334a1b830e70534bdea7e820983be0974f25aeee
Harith Anand <harith.kv47@gmail.com>
2022-04-12 16:23:34 +0530

    Delete BE/Basic-Jwt directory

--- BE/Basic-Jwt/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-
-//connect to DB
-const db = mongoose.connect(process.env.DB_CONNECT, () => console.log('connected to db!'))
-
-module.exports = db;
